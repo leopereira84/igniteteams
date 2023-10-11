c004162763514be6d89c2cbd2dd6a715c7ef9687
Leonardo Pereira <leonardo.pereira@itajai.sc.gov.br>
2023-10-11 17:47:03 -0300

    Criação de componentes usando CSS-in_JS

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
 import { StatusBar } from 'react-native';
-import { ThemeProvider } from 'styled-components';
+import { ThemeProvider } from 'styled-components/native';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
-import { Loading } from '@components/Loading';
-import { Groups } from '@screens/Groups';
 import theme from './src/theme';
+
+import { Loading } from '@components/Loading';
+import { Routes } from './src/routes';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
@@ -16,7 +17,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      { fontsLoaded ? <Groups /> : <Loading /> }
+      { fontsLoaded ? <Routes /> : <Loading /> }
     </ThemeProvider>
   );
 }
--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,15 +1,54 @@
-import { Header } from '@components/Header';
+import { useState } from 'react';
+import { FlatList } from 'react-native';
+import { useNavigation } from '@react-navigation/native'
+
 import { Container } from './styles';
+import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
+import { GroupCard } from '@components/GroupCard';
+import { ListEmpty } from '@components/ListEmpty';
+import { Button } from '@components/Button';
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>([]);
+
+  const navigation = useNavigation();
+
+  function handleNewGroup() {
+    navigation.navigate('groups');
+  }
+
   return (
     <Container>
       <Header />
+
       <Highlight
         title="Turmas"
         subtitle="Jogue com a sua turma"
       />
+
+      <FlatList
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <GroupCard
+            title={item}
+          />
+        )}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={() => (
+          <ListEmpty
+            message="Que tal cadastrar a primeira turma?"
+          />
+        )}
+        showsVerticalScrollIndicator={false}
+      />
+
+      <Button
+        title="Criar Nova Turma"
+        onPress={handleNewGroup}
+      />
+
     </Container>
   );
 }
